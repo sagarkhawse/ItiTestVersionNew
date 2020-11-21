93e38fde88a4bb3cd4b614a44df2396491928e05
Sagarkhawse <sagar.khawse@gmail.com>
2020-11-21 19:58:32 +0530

    Chapters added

--- app/src/main/java/com/skteam/ititest/restModel/home/subjects/Re.java
+++ app/src/main/java/com/skteam/ititest/restModel/home/subjects/Re.java
@@ -4,31 +4,55 @@ package com.skteam.ititest.restModel.home.subjects;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
 
 public class Re {
 
     @Expose
     private String image;
+
     @SerializedName("subject_id")
-    private String subjectId;
+    private int subjectId;
+
+    @SerializedName("chapter_id")
+    private int chapterId;
+
+    @SerializedName("test_id")
+    private int testId;
+
     @Expose
     private String title;
     @SerializedName("chapters_count")
     private String chapters;
 
+
+
+    @SerializedName("chapter_list")
+    private List<Re> chapterList;
+
+    @SerializedName("test_list")
+    private List<Re> testList;
+
     public String getImage() {
         return image;
+    }
+
+    public Re(int chapterId, String title, List<Re> testList) {
+        this.chapterId = chapterId;
+        this.title = title;
+        this.testList = testList;
     }
 
     public void setImage(String image) {
         this.image = image;
     }
 
-    public String getSubjectId() {
+    public int getSubjectId() {
         return subjectId;
     }
 
-    public void setSubjectId(String subjectId) {
+    public void setSubjectId(int subjectId) {
         this.subjectId = subjectId;
     }
 
@@ -46,5 +70,30 @@ public class Re {
 
     public void setChapters(String chapters) {
         this.chapters = chapters;
+    }
+
+    public List<Re> getChapterList() {
+        return chapterList;
+    }
+
+    public int getChapterId() {
+        return chapterId;
+    }
+
+    public int getTestId() {
+        return testId;
+    }
+
+    public List<Re> getTestList() {
+        return testList;
+    }
+
+    /**
+     *for populating chapter spinner
+     * @return title (chapter)
+     */
+    @Override
+    public String toString() {
+        return title;
     }
 }
--- app/src/main/java/com/skteam/ititest/ui/home/adapter/SubjectAdapter.java
+++ app/src/main/java/com/skteam/ititest/ui/home/adapter/SubjectAdapter.java
@@ -11,16 +11,23 @@ package com.skteam.ititest.ui.home.adapter;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.bumptech.glide.Glide;
 import com.skteam.ititest.R;
 import com.skteam.ititest.databinding.ItemSubjectBinding;
 import com.skteam.ititest.restModel.home.subjects.Re;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static com.skteam.ititest.setting.AppConstance.IMG_URL;
@@ -67,13 +74,57 @@ public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.SubjectH
 
         public void OnBinding(final Re re) {
             binding.tvTitle.setText(re.getTitle());
-            if(Integer.parseInt(re.getChapters())>0){
-                binding.chapterId.setText(re.getChapters()+" Chapters");
-            }else{
+            if (Integer.parseInt(re.getChapters()) > 0) {
+                binding.chapterId.setText(re.getChapters() + " Chapters");
+            } else {
                 binding.chapterId.setText("No Chapters");
             }
 
             Glide.with(context).load(IMG_URL + re.getImage()).into(binding.subjectImage);
+
+
+            // spinner adapter ( Data - Chapter list )
+            List<Re> chapterList = new ArrayList<>();
+            chapterList.add(new Re(-1, context.getString(R.string.select_chapter), null));
+            for (int i = 0; i < re.getChapterList().size(); i++) {
+                Re data = re.getChapterList().get(i);
+                chapterList.add(new Re(data.getChapterId(), data.getTitle(), data.getTestList()));
+            }
+            ArrayAdapter<Re> chaptersAdapter = new ArrayAdapter<>(context, android.R.layout.simple_list_item_1, chapterList);
+            binding.spinnerChapters.setAdapter(chaptersAdapter);
+
+
+            //spinner item selected listener
+            binding.spinnerChapters.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                    //on item selected populate test series recyclerview
+                    List<Re> testList = new ArrayList<>();
+                    testList = testList.get(i).getTestList();
+                    if (testList != null) {
+                        binding.rvTestSeries.setHasFixedSize(true);
+                        //TODO :- make test series adapter
+
+                    } else {
+                        //TODO show alert :- "Test Series will be updated soon"
+
+                    }
+
+                    //testing :- show test title
+                    //TODO :- remove this try catch code after making recyclerview
+                    try {
+                        Toast.makeText(context, "" + chapterList.get(i).getTestList().get(0).getTitle(), Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                        Toast.makeText(context, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+
+                }
+            });
         }
     }
 
